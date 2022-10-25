f69cd6161828996667e9da4296cbddfa3e3b1358
Muhammad Aji Solehudin <muhammadajisolehudin@gmail.com>
2022-10-25 11:21:15 +0700

    menambahkan assets

--- index.php
+++ index.php
@@ -5,11 +5,49 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <link rel="stylesheet" type="text/css" href="assets/styles/style.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    
+
+
     <title>Document</title>
 </head>
 <body>
-    
+    <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+        <img src="assets/images/logo1.png" width="30" height="30" alt="SENADA">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto" >
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Tentang Kami</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Konser Video</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Jadwal dan Perjalanan</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Produk</a>
+            </li>
+            <li class="nav-item">
+            <a class="nav-link" href="#">Kontak</a>
+            </li>
+        </ul>
+        </div>
+    </div>
+    </nav>
+
+    <div>
+        <h1 >ini tes</h1>
+    </div>
+ 
 </body>
 </html>
